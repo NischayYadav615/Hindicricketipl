4d7ed91ba3ea76b933115e9a02e2a0138e5cc1d3
NischayYadav615 <112478816+NischayYadav615@users.noreply.github.com>
2024-04-01 15:17:47 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 jwplayer("my-video").setup({
-	file: "https://prod-sports-hin-gm.jiocinema.com/hls/live/2100329/hd_akamai_androidmob_avc_hin_ipl_s2_m2310324/master.m3u8",
+	file: "videoURL",
 	image: "https://www.hbo.com/content/dam/hbodata/series/game-of-thrones/episodes/8/key-art/got-s8-ka-1920x1080.jpg/_jcr_content/renditions/cq5dam.web.1200.675.jpeg",
 	primary: "html5",
 	advertising: {
